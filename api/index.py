9c484d245d2f0c9794165596db40be311522c1c6
Andrew <82255762+AndrewBalin@users.noreply.github.com>
2023-05-03 11:58:30 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,16 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/ebash')
+@app.route('/')
 def mission():
     return "EBASH"
 
 
-def main():
-    db_session.global_init("db/main_db.db")
-    app.run()
 
-
-if __name__ == '__main__':
-    main()
-    app.run(port=5000, host='0.0.0.0')
